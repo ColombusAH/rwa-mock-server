76fbaf6f8e534ed4969b5f3d3e5b8ad51395c421
colombusAH <--add>
2022-11-12 00:50:03 +0200

    sync

--- server.ts
+++ server.ts
@@ -1,16 +1,36 @@
-import jsonServer from "json-server";
+
+import express from "express";
+import bodyParser from "body-parser";
 import { JsonDB, Config } from 'node-json-db';
+import * as JWT from 'jwt-simple';
+import { Article, User } from "./models";
 import { getArticles } from "./data/articles";
 import { getTags } from "./data/tags";
 import { getUsers } from "./data/users";
-import { Article, User } from "./models";
-import * as JWT from 'jwt-simple';
+
+
+const server = express();
+const jsonParser = bodyParser.json();
+server.use(jsonParser);
+
 const secret = 'supernotsecret';
-const middleware = jsonServer.defaults();
-const server = jsonServer.create();
-const db = new JsonDB(new Config("db.json", true, true, '/'));
-server.use(middleware);
-server.use(jsonServer.bodyParser);
+const db = new JsonDB(new Config("db.json", true, true, '/', true));
+
+
+const getUerMdlwr = async (req, res, next) => {
+	const auth = `${req.headers.authorization}`;
+	if (!auth) {
+		req.user = null;
+		return next();
+	}
+	const token = auth.split(' ')[1];
+	const tokenPayload = JWT.decode(token, secret);
+	const { email } = tokenPayload;
+	const users = await db.getObject<User[]>('/users') || [];
+	const user = users.find(u => u.email === email);
+	req.user = user;
+	next();
+};
 
 //users for test 
 server.get("/api/users", async (req, res) => {
@@ -45,7 +65,6 @@ server.post("/api/users/login", async (req, res) => {
 		const { password, ...userDetails } = user;
 		const tokenPayload = { email: user.email };
 		const token = JWT.encode(tokenPayload, secret);
-		console.log('the generated token is ', token);
 		return res
 			.status(200)
 			.send({ user: { ...userDetails, token } });
@@ -69,6 +88,26 @@ server.get("/api/articles", async (req, res) => {
 	res.status(200).send({ articles, articlesCount });
 });
 
+// articles -> like
+
+server.post("/api/articles/:slug/favorite", getUerMdlwr, async (req: any, res) => {
+	const slug = req.params.slug;
+	const articles = await db.getObject<Article[]>('/articles');
+	const users = await db.getObject<User[]>('/users');
+	console.log(req.user);
+	const requestedArticle = articles.find(article => article.slug === slug);
+	if (!requestedArticle) {
+		return res.status(404).send();
+	}
+	const user = users.find(u => u.email === req.user.email);
+	user.favoritesSlugs.push(requestedArticle.slug);
+	requestedArticle.favorited = true;
+	requestedArticle.favoritesCount += 1;
+	await db.push('/articles', articles, true);
+	await db.push('/users', users, true);
+	res.status(200).send({ requestedArticle });
+});
+
 //articles/feed
 
 server.get("/api/articles/feed", async (req, res) => {
@@ -78,30 +117,29 @@ server.get("/api/articles/feed", async (req, res) => {
 	res.status(200).send({ articles, articlesCount });
 });
 
+
 server.listen(3000, () => {
-	console.log(`Server listening on localhost:3000`);
+	console.log(`Server listening on http://localhost:3000`);
 });
+
 
 const init = () => {
 	db.push('/users', getUsers.users).then(async () => {
-		console.log('users loaded');
 		const users = await db.getObject<User[]>('/users');
-		console.log(users);
 	});
 
 	db.push('/articles', getArticles.articles).then(async () => {
-		console.log('articles loaded');
 		const articles = await db.getObject<Article[]>('/articles');
 	});
 
 	db.push('/tags', getTags.tags).then(async () => {
-		console.log('tags loaded');
 		const tags = await db.getObject<string[]>('/tags');
 	});
 
 
 
 };
+
 
 
 init();
